bb8a0622f3af19d5623436a3f437bf3d0395c100
Armin Roshan <aroshan@gamehouse.com>
2017-02-13 15:59:44 +0100

    Service, routing, Spt API added to the project.

--- spotifyApp/src/app/app.routing.ts
+++ spotifyApp/src/app/app.routing.ts
@@ -1,10 +1,10 @@
 import {Routes, RouterModule} from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+// import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
 
 const App_routes: Routes =[
-  { path: '', component: NavbarComponent },
+  // { path: '', component: NavbarComponent },
   { path: 'about', component: AboutComponent},
   { path: '', component: SearchComponent}
 ];
--- spotifyApp/src/app/components/search/search.component.ts
+++ spotifyApp/src/app/components/search/search.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../../../Artist';
 
 @Component({
   selector: 'search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [SpotifyService]
 })
 export class SearchComponent implements OnInit {
+  searchRes: Artist[];
+  searchStr: string;
 
-  constructor() { }
+  constructor(private _spotifyService: SpotifyService){
+
+  }
+
+  searchMusic(){
+    this._spotifyService.searchMusic(this.searchStr)
+      .subscribe(res => {
+        this.searchRes = res.artists.items;
+      })
+  }
+
 
   ngOnInit() {
   }
